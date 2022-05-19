754d71492554d0cc313c729ed97358f38653e8b7
a-camarillo <anthony.camarillo.96@gmail.com>
2022-05-19 12:57:31 -0700

    edit App component for use as base component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Header } from './Header';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header title="Anthony Camarillo" />
+      {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+  </header> */}
     </div>
   );
 }
