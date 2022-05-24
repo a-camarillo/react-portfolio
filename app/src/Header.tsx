7667f9f80740a4b5350dfd5da9366986ecda0fea
a-camarillo <anthony.camarillo.96@gmail.com>
2022-05-24 16:32:15 -0700

    remove FunctionComponent as it is discouraged

--- app/src/Header.tsx
+++ app/src/Header.tsx
@@ -1,9 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import './Header.css';
 import { NavBar } from './NavBar';
 import { TextProp } from './interfaces';
 
-export const Header: FunctionComponent<TextProp> = ({ title }) => 
+export const Header = ({ title }: TextProp) => 
     <div className="Header">    
         <h1>{ title }</h1>
             <NavBar items={["About Me", "Projects","Tools"]}/>
